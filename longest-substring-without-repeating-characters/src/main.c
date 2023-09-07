73e2e1220a7f762297bdbf5dc03d213fea6b0e07
Maxwell Babey <babeymax@gmail.com>
2023-09-07 10:23:35 -0400

    Add boilerplate and test cases

--- longest-substring-without-repeating-characters/src/main.c
+++ longest-substring-without-repeating-characters/src/main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
+int lengthOfLongestSubstring(char *s);
+
+int main(void)
 {
-    printf("Hello, World!\n");
+    char *input1;
+    char *input2;
+    char *input3;
+    
+    input1 = strdup("abcabcbb");
+    input2 = strdup("bbbbb");
+    input3 = strdup("pwwkew");
+    
+    printf("input1: %d\ninput2: %d\n input3: %d\n",
+           lengthOfLongestSubstring(input1),
+           lengthOfLongestSubstring(input2),
+           lengthOfLongestSubstring(input3));
+    
+    free(input1);
+    free(input2);
+    free(input3);
+    
+    return 0;
+}
+
+int lengthOfLongestSubstring(char *s)
+{
+
     return 0;
 }
