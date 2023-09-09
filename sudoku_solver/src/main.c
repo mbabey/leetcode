82153ec34ab149119212dc838cdd0cd7ff46262b
Maxwell Babey <babeymax@gmail.com>
2023-09-09 12:36:20 -0400

    Add test cases. Fix problems with memory

--- sudoku_solver/src/main.c
+++ sudoku_solver/src/main.c
@@ -3,6 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Test cases.
+//    [['6', '.', '.', '.', '.', '.', '8', '.', '3'],['.', '4', '.', '7', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '5', '.', '4', '.', '7', '.'],['3', '.', '.', '2', '.', '.', '.', '.', '.'],['1', '.', '6', '.', '.', '.', '.', '.', '.'],['.', '2', '.', '.', '.', '.', '.', '5', '.'],['.', '.', '.', '.', '8', '.', '6', '.', '.'],['.', '.', '.', '.', '1', '.', '.', '.', '.']]
+//    [['.', '.', '5', '.', '.', '.', '9', '8', '7'],['.', '4', '.', '.', '5', '.', '.', '.', '1'],['.', '.', '7', '.', '.', '.', '.', '.', '.'],['2', '.', '.', '.', '4', '8', '.', '.', '.'],['.', '9', '.', '1', '.', '.', '.', '.', '.'],['6', '.', '.', '2', '.', '.', '.', '.', '.'],['3', '.', '.', '6', '.', '.', '2', '.', '.'],['.', '.', '.', '.', '.', '9', '.', '7', '.'],['.', '.', '.', '.', '.', '.', '5', '.', '.']]
+//    [['.', '.', '.', '.', '.', '.', '.', '7', '1'],['.', '2', '.', '8', '.', '.', '.', '.', '.'],['.', '.', '.', '4', '.', '3', '.', '.', '.'],['7', '.', '.', '.', '6', '.', '.', '5', '.'],['.', '.', '.', '2', '.', '.', '3', '.', '.'],['9', '.', '.', '.', '.', '.', '.', '.', '.'],['6', '.', '.', '.', '7', '.', '.', '.', '.'],['.', '8', '.', '.', '.', '.', '4', '.', '.'],['.', '.', '.', '.', '5', '.', '.', '.', '.']]
+//    [['.', '4', '7', '.', '8', '.', '.', '.', '1'],['.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '6', '.', '.', '7', '.', '.'],['6', '.', '.', '.', '.', '3', '5', '7', '.'],['.', '.', '.', '.', '.', '5', '.', '.', '.'],['.', '1', '.', '.', '6', '.', '.', '.', '.'],['2', '8', '.', '.', '4', '.', '.', '.', '.'],['.', '9', '.', '1', '.', '.', '.', '4', '.'],['.', '.', '.', '.', '2', '.', '6', '9', '.']]
+//    [['.', '2', '.', '.', '.', '.', '.', '.', '.'],['3', '.', '5', '.', '6', '2', '.', '.', '9'],['.', '6', '8', '.', '.', '.', '3', '.', '.'],['.', '5', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '6', '4', '.', '8', '.', '2'],['.', '.', '4', '7', '.', '.', '9', '.', '.'],['.', '.', '3', '.', '.', '.', '.', '.', '1'],['.', '.', '.', '.', '.', '6', '.', '.', '.'],['1', '7', '.', '4', '3', '.', '.', '.', '.']]
+//    [['1', '.', '.', '.', '.', '6', '.', '8', '.'],['.', '6', '4', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '4', '.', '.', '.', '7'],['.', '.', '.', '.', '9', '.', '6', '.', '.'],['.', '7', '.', '4', '.', '.', '5', '.', '.'],['5', '.', '.', '.', '7', '.', '1', '.', '.'],['.', '5', '.', '.', '.', '.', '3', '2', '.'],['3', '.', '.', '.', '.', '8', '.', '.', '.'],['4', '.', '.', '.', '.', '.', '.', '.', '.']]
+//    [[".", ".", ".", "8", ".", ".", ".", ".", "9"],['.', '8', '7', '3', '.', '.', '.', '4', '.'],['6', '.', '.', '7', '.', '.', '.', '.', '.'],['.', '.', '8', '5', '.', '.', '9', '7', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '4', '3', '.', '.', '7', '5', '.', '.'],['.', '.', '.', '.', '.', '3', '.', '.', '.'],['.', '3', '.', '.', '.', '1', '4', '5', '.'],['4', '.', '.', '.', '.', '2', '.', '.', '1']]
+//    [[".", "4", ".", "5", ".", ".", ".", ".", "."],['8', '.', '.', '.', '9', '.', '.', '3', '.'],['.', '7', '6', '.', '2', '.', '.', '.', '.'],['.', '1', '4', '6', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '9', '.', '.', '7'],['.', '.', '.', '.', '.', '3', '6', '.', '.'],['.', '.', '1', '.', '.', '4', '.', '5', '.'],['.', '6', '.', '.', '.', '.', '.', '.', '3'],['.', '.', '7', '1', '.', '.', '2', '.', '.']]
+
+
 /**
  * Dimension of the grid.
  */
@@ -22,8 +33,9 @@
  * A single cell in the grid. Contains a value, whether the cell is set (i.e. locked in), and a pointer to
  * the next potential value for the cell. If set is true, then next must be NULL.
  */
-typedef struct node {
-    char value;
+typedef struct node
+{
+    char        value;
     bool set;
     struct node *next;
 } node_t;
@@ -32,7 +44,8 @@ typedef struct node {
  * A 9*9 grid of linked lists. Each linked list contains potential solutions for each cell
  * in the block.
  */
-struct block {
+struct block
+{
     node_t *subgrid[BOARD_SIZE];
     bool filled;
 };
@@ -40,7 +53,8 @@ struct block {
 /**
  * A 9*9 grid of struct blocks.
  */
-struct board {
+struct board
+{
     struct block *grid[BOARD_SIZE];
 };
 
@@ -100,6 +114,27 @@ void get_values_for_block(char **values, char **board, int block_num);
 
 int main(void)
 {
+    char board_static[9][9] = {
+            {'2', '6', '.', '.', '7', '.', '.', '.', '.'},
+            {'.', '.', '9', '6', '.', '2', '.', '1', '.'},
+            {'4', '.', '.', '3', '.', '.', '.', '.', '.'},
+            {'.', '.', '3', '.', '.', '.', '.', '.', '8'},
+            {'8', '.', '7', '9', '.', '4', '5', '.', '2'},
+            {'9', '.', '.', '.', '.', '.', '7', '.', '.'},
+            {'.', '.', '.', '.', '.', '7', '.', '.', '5'},
+            {'.', '4', '.', '2', '.', '6', '1', '.', '.'},
+            {'.', '.', '.', '.', '3', '.', '.', '8', '6'}};
+    
+    
+    int boardSize = BOARD_SIZE;
+    char **board = (char **) malloc(sizeof(char *) * BOARD_SIZE);
+    for (int i = 0; i < boardSize; ++i)
+    {
+        *(board + i) = (char *) malloc(boardSize);
+        memcpy(*(board + i), board_static[i], boardSize);
+    }
+    
+    solveSudoku(board, boardSize, &boardSize);
     
     return 0;
 }
@@ -117,16 +152,16 @@ void solveSudoku(char **board, int boardSize, int *boardColSize)
 
 struct board *create_board(char **board, int boardSize)
 {
-    struct board* board_d;
-    char *values;
+    struct board *board_d;
+    char         *values;
     
-    values = (char *) malloc(boardSize);
-    board_d = (struct board*) malloc(sizeof(struct board));
+    values  = (char *) malloc(boardSize);
+    board_d = (struct board *) malloc(sizeof(struct board));
     
     for (int block_num = 0; block_num < boardSize; ++block_num)
     {
         get_values_for_block(&values, board, block_num);
-    
+        
         *(board_d->grid + block_num) = create_block(values, boardSize);
     }
     
@@ -137,9 +172,9 @@ struct board *create_board(char **board, int boardSize)
 
 void get_values_for_block(char **values, char **board, int block_num)
 {
-    int  val_index;
-    int  col_bound;
-    int  row_bound;
+    int val_index;
+    int col_bound;
+    int row_bound;
     
     val_index = 0;
     col_bound = ((block_num % 3) + 1) * 3;
@@ -155,9 +190,9 @@ void get_values_for_block(char **values, char **board, int block_num)
 
 struct block *create_block(const char *values, int boardSize)
 {
-    struct block* block;
-    bool given;
-    bool prefilled;
+    struct block *block;
+    bool         given;
+    bool         prefilled;
     
     block = (struct block *) malloc(sizeof(struct block));
     
@@ -172,6 +207,8 @@ struct block *create_block(const char *values, int boardSize)
         }
     }
     block->filled = prefilled;
+    
+    return block;
 }
 
 node_t *create_node(char value, bool set, node_t *next)
@@ -180,22 +217,24 @@ node_t *create_node(char value, bool set, node_t *next)
     
     node = (node_t *) malloc(sizeof(node_t));
     node->value = value;
-    node->set = set;
-    node->next = next;
+    node->set   = set;
+    node->next  = next;
     
     return node;
 }
 
 void free_board(struct board *board)
 {
-    for (int i = 0; i < BOARD_SIZE; ++i) {
-        free_block((board->grid + i));
+    for (int i = 0; i < BOARD_SIZE; ++i)
+    {
+        free_block(*(board->grid + i));
     }
 }
 
 void free_block(struct block *block)
 {
-    for (int i = 0; i < BOARD_SIZE; ++i) {
+    for (int i = 0; i < BOARD_SIZE; ++i)
+    {
         free_list(*(block->subgrid + i));
     }
 }
