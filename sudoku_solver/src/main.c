75d04f4ab2927e5183386daff0ad7d8a5b173df7
Maxwell Babey <babeymax@gmail.com>
2023-09-11 15:18:11 -0400

    implement accept and reject functions

--- sudoku_solver/src/main.c
+++ sudoku_solver/src/main.c
@@ -37,17 +37,17 @@ void solveSudoku(char **board, int boardSize, int *boardColSize);
  * @param dst_board the original board in which to store the solution
  * @param boardSize the size of the board
  */
-char **solve(char **board, char **dst_board, int boardSize);
+char **solve(char **board, int row, int col, char **dst_board, int boardSize);
 
-int reject(char **board, int boardSize);
+int reject(char **board, int row, int col, int boardSize);
 
 int accept(char **board, int boardSize);
 
-int first(char t_board[9][9], char **board, int boardSize);
+int first(char t_board[9][9], int *col, int *row, char **board, int boardSize);
 
-int next(char board[9][9], int boardSize);
+int next(char board[9][9], int *row, int *col, int boardSize);
 
-char ** store_solution(char **board, char **dst_board, int boardSize);
+char **store_solution(char **board, char **dst_board, int boardSize);
 
 int main(void)
 {
@@ -102,29 +102,54 @@ void solveSudoku(char **board, int boardSize, int *boardColSize)
 //    free_board(board_d);
 }
 
-int reject(char **board, int boardSize)
+int reject(char **board, int row, int col, int boardSize)
 {
-
+    char c;
+    
+    c = *(*(board + row) + col);
+    
+    for (int i = 0; i < boardSize; ++i)
+    {
+        // Check the row for the same number. Check the col for the same number.
+        if (c == *(*(board + row) + i) || c == *(*(board + i) + col))
+        {
+            return 1;
+        }
+    }
+    
+    return 0;
 }
 
 int accept(char **board, int boardSize)
 {
-
+    // Check that the board contains no '.'
+    for (int r = 0; r < boardSize; ++r)
+    {
+        for (int c = 0; c < boardSize; ++c)
+        {
+            if (*(*(board + r) + c) == '.')
+            {
+                return 0;
+            }
+        }
+    }
+    
+    return 1;
 }
 
-int first(char t_board[BOARD_SIZE][BOARD_SIZE], char **board, int boardSize)
+int first(char t_board[9][9], int *col, int *row, char **board, int boardSize)
 {
 
 }
 
-int next(char board[BOARD_SIZE][BOARD_SIZE], int boardSize)
+int next(char board[9][9], int *row, int *col, int boardSize)
 {
 
 }
 
-char **solve(char **board, char **dst_board, int boardSize)
+char **solve(char **board, int row, int col, char **dst_board, int boardSize)
 {
-    if (reject(board, boardSize))
+    if (reject(board, row, col, boardSize))
     {
         return NULL;
     } else if (accept(board, boardSize))
@@ -132,21 +157,23 @@ char **solve(char **board, char **dst_board, int boardSize)
         return store_solution(board, dst_board, boardSize);
     }
     
-    int cont;
+    int  cont;
+    int t_row;
+    int t_col;
     char t_board[BOARD_SIZE][BOARD_SIZE];
-    cont = first(t_board, board, boardSize);
+    cont = first(t_board, &t_col, &t_row, board, boardSize);
     while (cont)
     {
-        if (solve((char **) t_board, dst_board, boardSize))
+        if (solve((char **) t_board, t_row, t_col, dst_board, boardSize))
         {
             return dst_board;
         }
-        cont = next(t_board, boardSize);
+        cont = next(t_board, &t_row, &t_col, boardSize);
     }
     return NULL;
 }
 
-char ** store_solution(char **board, char **dst_board, int boardSize)
+char **store_solution(char **board, char **dst_board, int boardSize)
 {
     
     return dst_board;
