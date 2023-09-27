8b4a5a32f1aa09cf048b149c8589acecaa2830f9
Maxwell Babey <babeymax@gmail.com>
2023-09-27 15:17:55 -0400

    Prelim solution for decoded string at index

---
+++ cpp/decoded-string-at-index/src/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    string decodeAtIndex(string s, int k)
+    {
+        string final;
+        
+        for (string::iterator it = s.begin(); it != s.end(); ++it)
+        {
+            // If the character read is a digit d, the entire current tape is repeatedly written d - 1 more times in total.
+            if (*it - '0' >= 0 && *it - '0' <= 9)
+            {
+                string current{final};
+                for (int i = 1; i < *it - '0'; ++i)
+                {
+                    final.append(current);
+                }
+            } else // If the character read is a letter, that letter is written onto the tape.
+            {
+                final.append(1, *it);
+            }
+        }
+        
+        // Second, get the char at the index. Use empty string if the index is greater than the string length.
+        string kth{((k >= final.length()) ?  "" : string(1, final.at(k)))};
+
+        return kth;
+    }
+};
+
+/*
+ * length is the null index
+ * if strlen = 10 & k = 10, str + k = \0
+ */
+
+int main()
+{
+    
+    Solution sol;
+    
+    string s1, s2, s3, s4, s5;
+    int    k1, k2, k3, k4, k5;
+    
+    s1 = "leet2code3";
+    k1 = 10;
+    s2 = "ha22";
+    k2 = 5;
+    s3 = "22";
+    k3 = 0;
+    s4 = "lb34";
+    k4 = 4;
+    s5 = "a2345678999999999999999";
+    k5 = 1;
+    
+    sol.decodeAtIndex(s1, k1); // Output: "leetleetcodeleetleetcodeleetleetcode", "o"
+    sol.decodeAtIndex(s2, k2); // Output: "hahahaha", "h"
+    sol.decodeAtIndex(s3, k3);
+    sol.decodeAtIndex(s4, k4);
+    sol.decodeAtIndex(s5, k5);
+    
+    return 0;
+}
+
