7849156d6bbb89f7ad7a390a9de43b1ba62b0433
Maxwell Babey <babeymax@gmail.com>
2023-09-27 13:53:25 -0400

    Add cpp solution to threesum

---
+++ cpp/threesum/src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+    
+    }
+};
+
+int main()
+{
+    Solution solution{};
+    
+    vector<int> nums;
+    nums.push_back(0);
+    nums.push_back(1);
+    nums.push_back(1);
+    
+    solution.threeSum(nums);
+    
+    return EXIT_SUCCESS;
+}
+
+
