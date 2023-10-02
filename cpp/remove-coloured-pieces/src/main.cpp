5632046bd866f6f0d6fbe5a7799ce0c8672f63c0
Maxwell Babey <babeymax@gmail.com>
2023-10-02 12:59:51 -0400

    Update solution for remove coloured pieces

--- cpp/remove-coloured-pieces/src/main.cpp
+++ cpp/remove-coloured-pieces/src/main.cpp
@@ -7,40 +7,36 @@ class Solution
 public:
     bool winnerOfGame(string colors)
     {
-        int alice_moves = 0, bob_moves = 0;
+        size_t len{colors.length()};
+        
+        if (len < 3)
+        {
+            return false; // Bob wins.
+        }
+        
+        int alice_moves{0}, bob_moves{0}, count;
         
         // Count the number of removable pieces for each player
-        for (size_t i{1}; i < colors.length() - 1; ++i) // O(n)
+        for (size_t i{1}; i < len; ++i) // O(n)
         {
-            if (colors.at(i - 1) == 'A' && colors.at(i) == 'A' && colors.at(i + 1) == 'A')
+            count = 0;
+            while (colors[i] == 'A' && colors[i] == colors[i - 1])
             {
-                ++alice_moves;
-            } else if (colors.at(i - 1) == 'B' && colors.at(i) == 'B' && colors.at(i + 1) == 'B')
-            {
-                ++bob_moves;
+                ++count;
+                ++i;
             }
+            alice_moves += max(0, count - 1);
+            count = 0;
+            while (colors[i] == 'B' && colors[i] == colors[i - 1])
+            {
+                ++count;
+                ++i;
+            }
+            bob_moves += max(0, count - 1);
         }
         
-        // Set the winner to alice
-        bool turn = true;
-        
-        // While on their turn there are pieces to remove for alice or pieces to remove for bob,
-        while ((alice_moves && turn) || (bob_moves && !turn)) // O(n - m), so O(n)
-        {
-            // Alternate decrementing alice and bob's number of removable pieces.
-            if (turn)
-            {
-                --alice_moves;
-                
-            } else
-            {
-                --bob_moves;
-            }
-            turn = !turn;
-        }
-        
-        // The winner is the player after the player who does not play on their turn.
-        return !turn;
+        // Alice wins if she has more moves than Bob; otherwise, Bob wins.
+        return (alice_moves > bob_moves);
     }
 };
 
@@ -55,7 +51,7 @@ int main()
     string game3{"ABBBBBBBAAA"};
     string game4{"AAA"};
     string game5{"ABABABABABABA"};
-    string game6{"BBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAAAAAA"}; // 25 to 26
+    string game6{"BBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAAAAA"}; // 25 to 26
     
     cout << "Winner: " << bool_to_string(sol.winnerOfGame(game1)) << endl;
     cout << "Winner: " << bool_to_string(sol.winnerOfGame(game2)) << endl;
