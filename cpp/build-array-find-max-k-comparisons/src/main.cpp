854a9782b5ace6a0265898d10cc3d9101cd6807e
Maxwell Babey <babeymax@gmail.com>
2023-10-07 15:24:46 -0400

    Attempt solution for build array find max k comparisons

---
+++ cpp/build-array-find-max-k-comparisons/src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int numOfArrays(int n, int m, int k) {
+        long ans{0};
+        
+        if (m < k || k == 0) // No satisfactory array can be constructed
+        {
+            return 0;
+        }
+        
+        vector<vector<vector<long>>> dp;
+        for (int)
+        
+        
+        return static_cast<int>(ans % 1000000007);
+    }
+};
+
+int main()
+{
+    
+    
+    return EXIT_SUCCESS;
+}
